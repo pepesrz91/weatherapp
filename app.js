ac8143c7c74c2a0ce1dc2e7cbd395ee9d45bdb7c
DESKTOP-2UG043F\pepe_ <pepesrzra@outlook.com>
2018-07-05 15:42:05 -0500

    Before creating new directory for Dark Sky

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-const yargs = require('yargs');
-const geocode = require('./geocode/geocode');
-
-const arg = yargs.options({
-a: {
-    demand:true,
-    alias:"adsress",
-    describe:"Address to fetech weather for",
-    string:true
-  }
-})
-.help()
-.alias("help","h")
-.argv;
-
-geocode.geocodeAddress(arg.a,(errorMessage,results)=>{
-  if (errorMessage) {
-    console.log(errorMessage);
-  }else {
-    //console.log(results);
-    console.log(JSON.stringify(results,undefined,2));
-  }
-});
+// const yargs = require('yargs');
+// const geocode = require('./geocode/geocode');
+//
+// const arg = yargs.options({
+// a: {
+//     demand:true,
+//     alias:"adsress",
+//     describe:"Address to fetech weather for",
+//     string:true
+//   }
+// })
+// .help()
+// .alias("help","h")
+// .argv;
+//
+// geocode.geocodeAddress(arg.a,(errorMessage,results)=>{
+//   if (errorMessage) {
+//     console.log(errorMessage);
+//   }else {
+//     //console.log(results);
+//     console.log(JSON.stringify(results,undefined,2));
+//   }
+// });
 
 //API KEY DARK SKY 152f487d54c1bbd103842b4651636ca7
 //EXAMPLE FOR BROWSER
@@ -32,5 +32,13 @@ request({
   url:"https://api.darksky.net/forecast/152f487d54c1bbd103842b4651636ca7/20.6596988,-103.3496092",
   json:true
 },(error,response,body)=>{
-  console.log(JSON.stringify(body.currently,undefined,2));
+  if (error) {
+    console.log('Unable to connect to Forecaste.ios server');
+  }else if (response.statusCode === 404)  {
+    console.log('Unable to fetch the weather');
+  }else if (response.statusCode === 200) {
+    console.log('Everything working awesome!!!');
+    console.log(JSON.stringify(body.currently,undefined,2));
+  }
+
 });
