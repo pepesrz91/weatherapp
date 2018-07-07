41845055bb2577a1aeb1584a48624ad046dade3a
DESKTOP-2UG043F\pepe_ <pepesrzra@outlook.com>
2018-07-07 12:38:34 -0500

    Advanced Promise

--- playground/promise.js
+++ playground/promise.js
@@ -1,11 +1,30 @@
-var somePromise = new Promise((resolve,reject)=>{
-  setTimeout(()=>{
-    resolve('Hey.It worked');
-  },3000);
-});
+var asyncAdd = (a,b)=>{
+  return new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+      if (typeof a === 'number' && typeof b === 'number') {
+        resolve(a + b);
+      } else {
+        reject('Arguments must be numbers');
+      }
+    },1500);
+  });
+};
 
-somePromise.then((message)=>{
-  console.log(`Sucess: ${message}`);
+asyncAdd('Hola',456).then((res)=>{
+  console.log('We got some numbers!');
+  console.log(`The sum is ${res}`);
 },(errorMessage)=>{
-  console.log(`Error: ${errorMessage}`);
+  console.log(`Only numbers you dummy!`);
+  console.log(`${errorMessage}`);
 });
+// var somePromise = new Promise((resolve,reject)=>{
+//   setTimeout(()=>{
+//     resolve('Hey.It worked');
+//   },3000);
+// });
+//
+// somePromise.then((message)=>{
+//   console.log(`Sucess: ${message}`);
+// },(errorMessage)=>{
+//   console.log(`Error: ${errorMessage}`);
+// });
