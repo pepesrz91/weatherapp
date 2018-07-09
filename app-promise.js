861ca019780c72f57ace2b0b5296d1cbabf6b55f
DESKTOP-2UG043F\pepe_ <pepesrzra@outlook.com>
2018-07-09 14:03:51 -0500

    Projec using axios for promises.

--- app-promise.js
+++ app-promise.js
@@ -35,28 +35,26 @@ var encodedAddress = encodeURIComponent(argv.address);
 var geocodeUrl =  `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`
 
 axios.get(geocodeUrl).then((response)=>{
-  console.log(response.data);
-}).catch((err)=>{
-  console.log(err.code);
+  if (response.data.status === 'ZERO_RESULTS') {
+    throw new Error('Unable to find that Address');
+  }
+  var formattedAddress = response.data.results[0].formatted_address;
+
+  var lat = response.data.results[0].geometry.location.lat;
+  var lng = response.data.results[0].geometry.location.lat;
+  var weatherUrl = `https://api.darksky.net/forecast/152f487d54c1bbd103842b4651636ca7/${lat},${lng}`;
+  console.log(formattedAddress);
+  return axios.get(weatherUrl);
+
+}).then((response)=>{
+  var temperature = response.data.currently.temperature;
+  var apparentTemperature = response.data.currently.apparentTemperature;
+  console.log(`It's currently ${temperature}. It feels like ${apparentTemperature}`);
+
+}).catch((e)=>{
+  if (e.code === 'ENOTFOUND') {
+    console.log('Unable to connect to API servers');
+  }else{
+    console.log(e.message);
+  }
 });
-
-//API KEY DARK SKY 152f487d54c1bbd103842b4651636ca7
-//EXAMPLE FOR BROWSER
-//https://api.darksky.net/forecast/152f487d54c1bbd103842b4651636ca7/20.6596988,-103.3496092
-
-// const request = require('request');
-//
-// request({
-//   url:"https://api.darksky.net/forecast/152f487d54c1bbd103842b4651636ca7/20.6596988,-103.3496092",
-//   json:true
-// },(error,response,body)=>{
-//   if (error) {
-//     console.log('Unable to connect to Forecaste.ios server');
-//   }else if (response.statusCode === 404)  {
-//     console.log('Unable to fetch the weather');
-//   }else if (response.statusCode === 200) {
-//     console.log('Everything working awesome!!!');
-//     console.log(JSON.stringify(body.currently,undefined,2));
-//   }
-//
-// });
